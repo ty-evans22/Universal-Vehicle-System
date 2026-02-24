c986a8dc57ee11a867d22ef93e1e2eaefd9a4369
Tyler <tevans.dev@gmail.com>
2026-02-24 17:53:40 -0600

    Added header file structure for project.

--- Source/UniversalVehicleSystem/UniversalVehicleSystem.Build.cs
+++ Source/UniversalVehicleSystem/UniversalVehicleSystem.Build.cs
@@ -26,7 +26,10 @@ public class UniversalVehicleSystem : ModuleRules
 			new string[]
 			{
 				"Core",
-				// ... add other public dependencies that you statically link with here ...
+				"CoreUObject",
+				"Engine",
+				"InputCore",
+				"EnhancedInput"
 			}
 			);
 			
